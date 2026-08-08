b8f9246b9f9c6624e7fb413538985f363391812d
Neale Matthew Nicolas <neale.nicolas@gmail.com>
2026-08-08 04:05:52 +0000

    updated py files with code templates

---
+++ Alpha1/Birdie/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    city = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
+class SavedPokemon(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="savedPokemon"
+    )
+
+    pokemon_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    pokemon_types = models.CharField(max_length=100)
+    image = models.URLField(blank=True)
+
+    saved_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
---
+++ Alpha1/Birdie/views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
+
+#Import requests library to execute http transactions 
+import requests
+
+#Import to update profile 
+from .forms import UserUpdateForm, ProfileUpdateForm
+from .models import Profile , SavedPokemon
+
+
+
+# Create your views here.
+
+#Method for rendering home page
+def home(request): 
+	return render(request,'home.html',{})
+
+#Method for rendering about page
+@login_required
+def about(request): 
+	return render(request,'about.html',{})
+
+#Method for register a new user
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request,"Account created succesfully")
+			print("New user was created")
+			return redirect("login")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "register.html",{"form":form})
+
+
+#Method for C-RUD (Read, Update and Delete) the user profile
+@login_required
+def profile(request):
+    profile_object, created = Profile.objects.get_or_create(
+        user=request.user
+    )
+
+    if request.method == "POST":
+        user_form = UserUpdateForm(
+            request.POST,
+            instance=request.user,
+        )
+
+        profile_form = ProfileUpdateForm(
+            request.POST,
+            instance=profile_object,
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            messages.success(
+                request,
+                "Your profile was updated successfully.",
+            )
+
+            return redirect("profile")
+
+    else:
+        user_form = UserUpdateForm(
+            instance=request.user
+        )
+
+        profile_form = ProfileUpdateForm(
+            instance=profile_object
+        )
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    }
+
+    return render(
+        request,
+        "profile.html",
+        context,
+    )
+
+
+def pokemon_search(request):
+    pokemon_data = None
+    error = None 
+
+    if request.method == "POST":
+        pokemon= request.POST.get("pokemon","").strip().lower()
+
+        if pokemon:
+            url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                
+                image_url = data["sprites"].get("front_defalut")
+
+                pokemon_data = {
+                    "id":data["id"],
+                    "name":data["name"],
+                    "types":[
+                        item["type"]["name"]
+                        for item in data["types"]
+                        ],
+                    "image": image_url,
+                        }
+
+            else: 
+                error = "Pokemon not found"    
+    return render(
+        request,
+        "search.html",
+        {
+            "pokemon_data" : pokemon_data,
+            "error" : error,
+        },
+    )
