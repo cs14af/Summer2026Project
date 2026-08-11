45ea875b739e75bf03f0a8fff895b5144e3639e5
Neale Matthew Nicolas <neale.nicolas@gmail.com>
2026-08-11 14:00:39 +0000

    successful combining of our current work on homepage, navbar, user registration, auth, profile, tests

--- Alpha1/Birdie/urls.py
+++ Alpha1/Birdie/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Birdie import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', views.home_view, name='Home'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
