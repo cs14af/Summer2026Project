eb44d68ac927a412222b3de3439e2a43f5d3e991
Alejandro Fernandez <alejandro.fernandez05@utrgv.edu>
2026-08-21 08:24:59 +0000

    server is running on supabase

--- Alpha1/Alpha1/settings.py
+++ Alpha1/Alpha1/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -28,9 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://localhost:8000",
-]
+
 
 # Application definition
 
